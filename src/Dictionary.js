4778478235be74d4172dbeddcda7a0dee7a798d9
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-05-10 17:08:11 -0400

    Added Results and Meanings Components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function keywordChange(event) {
     setKeyword(event.target.value);
   }
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -30,6 +32,7 @@ export default function Dictionary() {
         />
         <input type="submit" value="Search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+  console.log(props.results);
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        <h3 className="Definition">
+          {props.results.meanings.map(function(meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </h3>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
