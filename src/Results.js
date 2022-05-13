84613294a0881fbfc15ee59bffb073917755dced
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-05-12 22:18:08 -0400

    Styling

--- src/Results.js
+++ src/Results.js
@@ -4,34 +4,33 @@ import "./Results.css";
 import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="word">{props.results.word}</h2>
-        <div className="phoneticsAudio">
-          {props.results.phonetics.map(function(phonetics, index) {
-            return (
-              <div key={index}>
-                <Phonetics phonetics={phonetics} />
-              </div>
-            );
-          })}
-        </div>
-        <div>{props.results.phonetic}</div>
-        <div className="card">
-          <div className="card-body">
-            <h3 className="Definition">
-              {props.results.meanings.map(function(meaning, index) {
-                return (
-                  <div key={index}>
-                    <Meaning meaning={meaning} />
-                  </div>
-                );
-              })}
-            </h3>
+        <h1 className="word">{props.results.word}</h1>
+        <section>
+          <div className="phoneticsAudio">
+            {props.results.phonetics.map(function(phonetics, index) {
+              return (
+                <div key={index}>
+                  <Phonetics phonetics={phonetics} />
+                </div>
+              );
+            })}
           </div>
-        </div>
+          <div>{props.results.phonetic}</div>
+        </section>
+        <section>
+          <h3 className="Definition">
+            {props.results.meanings.map(function(meaning, index) {
+              return (
+                <div key={index}>
+                  <Meaning meaning={meaning} />
+                </div>
+              );
+            })}
+          </h3>
+        </section>
       </div>
     );
   } else {
